ec673d32ccb9fd3d699bfa78c544dc1ffa09ed41
Santiago Firpo <s.firpo@edu.pucrs.br>
2022-12-11 19:08:45 -0300

    Added Sources

---
+++ Source/Ignotus/IgnotusGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IgnotusGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AIgnotusGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AIgnotusGameMode();
+};
+
+
+
